5c29b7670a9fe6a6a301f297a830d8ff1daed313
ZholudV <goldexzholud@gmail.com>
2022-09-09 05:11:44 +0500

    добавил User и UserDao

---
+++ src/main/java/ru/zholud/homeworke5/hibernate1/User.java
@@ -0,0 +1,47 @@
+package ru.zholud.homeworke5.hibernate1;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User() {
+    }
+
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
